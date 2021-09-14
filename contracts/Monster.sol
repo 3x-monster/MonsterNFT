effa55b544e316caabee63d711ce70ada7e9bdd9
thlynn <hlxstc680@gmail.com>
2021-09-14 10:36:48 +0800

    fix: make this contract payable

--- contracts/Monster.sol
+++ contracts/Monster.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Loot is ERC721 {
 
-    address public owner;
+    address payable public owner;
 
     string[] private monsters = [
         "Dragons"
@@ -56,7 +56,7 @@ contract Loot is ERC721 {
     mapping(uint => bool) public fexists;
 
     constructor() ERC721("Monster Manifested", "MMS"){
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
 
     modifier onlyOwner() {
@@ -72,7 +72,9 @@ contract Loot is ERC721 {
         return prefixes[rand % prefixes.length];
     }
 
-    function mintMonster() internal {
+    function mintMonster() public payable{
+        require(msg.value == 10e18, "10FTM IS REQUIRED");
+
         next_monster ++;
         uint _next_monster = next_monster;
 
@@ -126,45 +128,57 @@ contract Loot is ERC721 {
     }
 
     function tokenURI(uint _token_id) override public view returns (string memory) {
-        string[17] memory parts;
+        string[21] memory parts;
+
         parts[0] = '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350"><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">';
 
-        parts[1] = monster[_token_id];
+        parts[1] = string(abi.encodePacked("monster", " ", prefix[_token_id], " ", monster[_token_id], " ", suffix[_token_id]));
 
         parts[2] = '</text><text x="10" y="40" class="base">';
 
-        parts[3] = prefix[_token_id];
+        parts[3] = string(abi.encodePacked("health_Point", " ", health_Point[_token_id]));
 
         parts[4] = '</text><text x="10" y="60" class="base">';
 
-        parts[5] = suffix[_token_id];
+        parts[5] = string(abi.encodePacked("physical_damage_point", " ", physical_damage_point[_token_id]));
 
-        // parts[6] = '</text><text x="10" y="80" class="base">';
+        parts[6] = '</text><text x="10" y="80" class="base">';
 
-        // parts[7] = getWaist(tokenId);
+        parts[7] = string(abi.encodePacked("magical_damage_point", " ", magical_damage_point[_token_id]));
 
-        // parts[8] = '</text><text x="10" y="100" class="base">';
+        parts[8] = '</text><text x="10" y="100" class="base">';
 
-        // parts[9] = getFoot(tokenId);
+        parts[9] = string(abi.encodePacked("physical_defence", " ", physical_defence[_token_id]));
 
-        // parts[10] = '</text><text x="10" y="120" class="base">';
+        parts[10] = '</text><text x="10" y="120" class="base">';
 
-        // parts[11] = getHand(tokenId);
+        parts[11] = string(abi.encodePacked("magical_defence", " ", magical_defence[_token_id]));
 
-        // parts[12] = '</text><text x="10" y="140" class="base">';
+        parts[12] = '</text><text x="10" y="140" class="base">';
 
-        // parts[13] = getNeck(tokenId);
+        parts[13] = string(abi.encodePacked("dodge", " ", dodge[_token_id]));
 
-        // parts[14] = '</text><text x="10" y="160" class="base">';
+        parts[14] = '</text><text x="10" y="160" class="base">';
 
-        // parts[15] = getRing(tokenId);
+        parts[15] = string(abi.encodePacked("hit", " ", hit[_token_id]));
 
-        parts[16] = '</text></svg>';
+        parts[16] = '</text><text x="10" y="160" class="base">';
 
-        string memory output = string(abi.encodePacked(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], parts[8]));
-        output = string(abi.encodePacked(output, parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], parts[15], parts[16]));
+        parts[17] = string(abi.encodePacked("critical", " ", critical[_token_id]));
         
-        string memory json = Base64.encode(bytes(string(abi.encodePacked('{"name": "Bag #', toString(_token_id), '", "description": "Loot is randomized adventurer gear generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Loot in any way you want.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
+        parts[18] = '</text><text x="10" y="160" class="base">';
+
+        parts[19] = string(abi.encodePacked("parry", " ", parry[_token_id]));
+
+        parts[20] = '</text></svg>';
+
+        string memory output = string(abi.encodePacked(
+            parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6], parts[7], 
+            parts[8], parts[9], parts[10], parts[11], parts[12], parts[13], parts[14], 
+            parts[15], parts[16], parts[17], parts[18], parts[19], parts[20]));
+        
+        string memory json = Base64.encode(bytes(string(
+            abi.encodePacked('{"name": "Bag #', toString(_token_id), '", "description": "Loot is randomized adventurer gear generated and stored on chain. Stats, images, and other functionality are intentionally omitted for others to interpret. Feel free to use Loot in any way you want.", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(output)), '"}'))));
         output = string(abi.encodePacked('data:application/json;base64,', json));
 
         return output;
@@ -200,6 +214,13 @@ contract Loot is ERC721 {
             value /= 10;
         }
         return string(buffer);
+    }
+
+    function withdraw() public onlyOwner{
+        uint amount = address(this).balance;
+
+        (bool success, ) = owner.call{value: amount}("");
+        require(success, "Failed to send Ether");
     }
 
 }
